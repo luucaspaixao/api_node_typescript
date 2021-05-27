448a65fe81be32f58f6f83de0a63578043a97193
Lucas Paixão <46897200+luucaspaixao@users.noreply.github.com>
2021-05-27 12:29:18 -0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  return response.status(200).json({ message: "Hello World" });
+});
+
+app.listen(3333);
